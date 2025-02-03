01b2a9b22e223ce152dfb763f115e099493dca3e
Axel Rizo <axelg.rizos@gmail.com>
2025-02-03 17:39:50 -0500

    chore: rename main and router files to supahoot-web structure with path aliases

--- src/lib/supahoot-web/main.ts
+++ src/lib/supahoot-web/main.ts
@@ -1,8 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@supahoot-web/App.vue'
+import router from '@supahoot-web/router'
 
 const app = createApp(App)
 

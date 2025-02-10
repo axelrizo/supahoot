ffb24f5e8db5175f452db8f43df887c35c97fcf8
Axel Rizo <axelg.rizos@gmail.com>
2025-02-10 01:29:36 -0500

    refactor: simplify lobby-related test helpers for improved readability

--- src/test/features/user-can-create-a-lobby.test.ts
+++ src/test/features/user-can-create-a-lobby.test.ts
@@ -1,9 +1,4 @@
-import {
-  CREATE_LOBBY_BUTTON,
-  LOBBY_FORM,
-  LOBBY_NAME_INPUT,
-  LOBBY_TITLE,
-} from '@/test/support/lobby-helpers'
+import { lobbyHelpers } from '@/test/support/lobby-helpers'
 import HomeView from '@supahoot-web/views/HomeView.vue'
 import { mount } from '@vue/test-utils'
 
@@ -11,9 +6,9 @@ test('user can create a lobby', () => {
   const lobbyName = 'My Lobby'
   const wrapper = mount(HomeView)
 
-  wrapper.find(CREATE_LOBBY_BUTTON).trigger('click')
-  wrapper.find(LOBBY_NAME_INPUT).setValue(lobbyName)
-  wrapper.find(LOBBY_FORM).trigger('submit')
+  wrapper.find(lobbyHelpers.createButton).trigger('click')
+  wrapper.find(lobbyHelpers.nameInput).setValue(lobbyName)
+  wrapper.find(lobbyHelpers.createForm).trigger('submit')
 
-  expect(wrapper.find(LOBBY_TITLE).text()).toContain(lobbyName)
+  expect(wrapper.find(lobbyHelpers.card).text()).toContain(lobbyName)
 })
--- src/test/support/lobby-helpers.ts
+++ src/test/support/lobby-helpers.ts
@@ -1,6 +1,9 @@
 import { testId } from '@/test/support/test-utils'
 
-export const CREATE_LOBBY_BUTTON = testId('create-lobby-button')
-export const LOBBY_NAME_INPUT = testId('lobby-name-input')
-export const LOBBY_FORM = testId('lobby-form')
-export const LOBBY_TITLE = testId('lobby-name')
+export const lobbyHelpers = {
+  createButton: testId('create-lobby-button'),
+  nameInput: testId('lobby-name-input'),
+  createForm: testId('lobby-form'),
+  card: testId('lobby-card'),
+  createModal: testId('lobby-create-modal'),
+}

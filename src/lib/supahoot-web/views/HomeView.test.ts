17b2dea4fdcebcb65c471eb473a17855d3505cb4
Axel Rizo <axelg.rizos@gmail.com>
2025-02-10 01:29:42 -0500

    feat: implement lobby creation modal and update HomeView tests

--- src/lib/supahoot-web/views/HomeView.test.ts
+++ src/lib/supahoot-web/views/HomeView.test.ts
@@ -1,4 +1,4 @@
-import { LOBBY_FORM } from '@/test/support/lobby-helpers'
+import { lobbyHelpers } from '@/test/support/lobby-helpers'
 import { container } from '@/test/support/setup-container-mock'
 import HomeView from '@supahoot-web/views/HomeView.vue'
 import { mount } from '@vue/test-utils'
@@ -7,8 +7,22 @@ describe('HomeView', () => {
   test('success: connect with service when submit form', () => {
     const wrapper = mount(HomeView)
 
-    wrapper.find(LOBBY_FORM).trigger('submit')
+    wrapper.find(lobbyHelpers.createForm).trigger('submit')
 
     expect(container.lobbyService.create).toHaveBeenCalled()
+  })
+
+  test('success: lobby form should be hidden', () => {
+    const wrapper = mount(HomeView)
+
+    expect(wrapper.find(lobbyHelpers.createModal).attributes('class')).toContain('hidden')
+  })
+
+  test('success: lobby form should be visible when user click on create lobby button', async () => {
+    const wrapper = mount(HomeView)
+
+    await wrapper.find(lobbyHelpers.createButton).trigger('click')
+
+    expect(wrapper.find(lobbyHelpers.createModal).attributes('class')).not.toContain('hidden')
   })
 })
--- src/lib/supahoot-web/views/HomeView.vue
+++ src/lib/supahoot-web/views/HomeView.vue
@@ -5,17 +5,25 @@ import { inject, ref } from 'vue'
 const { lobbyService } = inject('container') as { lobbyService: LobbyService }
 
 const lobbyName = ref('')
+const isFormVisible = ref(false)
 
 const createLobby = () => {
   lobbyService.create()
 }
+const openCreateLobbyModal = () => {
+  isFormVisible.value = true
+}
 </script>
 
 <template>
-  <button data-testid="create-lobby-button"></button>
-  <form data-testid="lobby-form" v-on:submit="createLobby">
-    <input type="text" data-testid="lobby-name-input" v-model="lobbyName" />
-    <input type="submit" />
-  </form>
-  <div data-testid="lobby-name">My Lobby</div>
+  <button data-testid="create-lobby-button" v-on:click="openCreateLobbyModal">a</button>
+
+  <div data-testid="lobby-create-modal" :class="{ hidden: !isFormVisible }">
+    <form data-testid="lobby-form" v-on:submit="createLobby">
+      <input type="text" data-testid="lobby-name-input" v-model="lobbyName" />
+      <input type="submit" />
+    </form>
+  </div>
+
+  <div data-testid="lobby-card">My Lobby</div>
 </template>

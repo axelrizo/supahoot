6c23419a494fd759707086542e3625a544c48c59
Axel Rizo <axelg.rizos@gmail.com>
2025-02-10 00:21:27 -0500

    feat: provide service container in App.vue

--- src/lib/supahoot-web/App.vue
+++ src/lib/supahoot-web/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { container } from '@supahoot/services/container'
+import { provide } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+
+provide('container', container)
 </script>
 
 <template>

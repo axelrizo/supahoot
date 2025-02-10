a2d86bf0f8279b78772e7ba231c6bad84dbd52ae
Axel Rizo <axelg.rizos@gmail.com>
2025-02-10 00:21:59 -0500

    test: add utility functions and lobby-related test IDs

---
+++ src/test/support/lobby-helpers.ts
@@ -0,0 +1,6 @@
+import { testId } from '@/test/support/test-utils'
+
+export const CREATE_LOBBY_BUTTON = testId('create-lobby-button')
+export const LOBBY_NAME_INPUT = testId('lobby-name-input')
+export const LOBBY_FORM = testId('lobby-form')
+export const LOBBY_TITLE = testId('lobby-name')

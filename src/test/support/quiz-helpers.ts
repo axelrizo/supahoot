03d6fbda76a4085e9ad7fc66265503e15b86f854
Axel Rizo <axelg.rizos@gmail.com>
2025-03-09 01:51:33 -0500

    refactor(tests): update testId imports and consolidate utility functions

--- src/test/support/quiz-helpers.ts
+++ src/test/support/quiz-helpers.ts
@@ -1,14 +1,6 @@
-import type { VueWrapper } from '@vue/test-utils'
-import { testId } from './test-utils'
+import { testId } from './utils/html-utils'
 
-const fillSecretWordAndSubmit = async (wrapper: VueWrapper, secretWord: string) => {
-  wrapper.find(quizHelpers.secretWordInput).setValue(secretWord)
-  await wrapper.find(quizHelpers.verifyButton).trigger('click')
-}
 
 export const quizHelpers = {
-  secretWordInput: testId('secret-word-input'),
-  verifyButton: testId('verify-secret-word-button'),
-  createQuizButton: testId('create-quiz-button'),
-  fillSecretWordAndSubmit,
+  quizTitle: testId('quiz-title'),
 }

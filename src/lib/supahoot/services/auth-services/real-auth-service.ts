e833a782bd858d5ccd82ddbf01d175324ddf2fab
Axel Rizo <axelg.rizos@gmail.com>
2025-03-09 00:32:52 -0500

    fix(auth): update environment variable access method in RealAuthService

--- src/lib/supahoot/services/auth-services/real-auth-service.ts
+++ src/lib/supahoot/services/auth-services/real-auth-service.ts
@@ -2,6 +2,6 @@ import type { AuthService } from "../auth-service";
 
 export class RealAuthService implements AuthService {
   verifyAdminSecretWord(secretWord: string) {
-    return secretWord === process.env.VITE_ADMIN_SECRET_WORD
+    return secretWord === import.meta.env.VITE_ADMIN_SECRET_WORD;
   }
 }

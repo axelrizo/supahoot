d16a2386a7b47d191e8af64b9ad209af36c196ad
Axel Rizo <axelg.rizos@gmail.com>
2025-03-09 00:28:18 -0500

    feat: implement AdminView with secret word verification and notifications

---
+++ src/lib/supahoot-web/views/AdminView.test.ts
@@ -0,0 +1,30 @@
+import { quizHelpers } from "@/test/support/quiz-helpers"
+import { container, notificationProvider } from "@/test/support/setup-container-mock"
+import { mount, VueWrapper } from "@vue/test-utils"
+import AdminView from "./AdminView.vue"
+
+let wrapper: VueWrapper
+
+describe("AdminView", () => {
+  beforeEach(() => {
+    wrapper = mount(AdminView)
+  })
+
+  test("success: redirect to new view when correct word", async () => {
+    container.authService.verifyAdminSecretWord.mockReturnValue(true)
+
+    await quizHelpers.fillSecretWordAndSubmit(wrapper, "DUMMY_WORD")
+
+    expect(wrapper.vm.$route.path).toBe("/admin/init-quiz")
+  })
+
+  test("error: send error when secret is incorrect", async () => {
+    container.authService.verifyAdminSecretWord.mockReturnValue(false)
+
+    await quizHelpers.fillSecretWordAndSubmit(wrapper, "DUMMY_WORD")
+
+    expect(notificationProvider.showNotification).toHaveBeenCalledWith(
+      'Error: Incorrect secret word',
+    )
+  })
+})

e9c3708afc2cd39915f71b8f02d306a7ceae5fa6
Axel Rizo <axelg.rizos@gmail.com>
2025-03-12 23:31:03 -0500

    fix(home): remove HomeView because Admin will handle that now

--- src/lib/supahoot/services/container.ts
+++ src/lib/supahoot/services/container.ts
@@ -1,14 +1,10 @@
-import type { LobbyService } from '@supahoot/services/lobby-service'
-import { SupabaseLobbyService } from '@supahoot/services/lobby-services/supabase-lobby-service'
 import type { QuizService } from '@supahoot/services/quiz-service'
 import { SupabaseQuizService } from '@supahoot/services/quiz-services/supabase-quiz-service'
 
 export interface ServicesContainer {
-  lobbyService: LobbyService
   quizService: QuizService
 }
 
 export const container: ServicesContainer = {
-  lobbyService: new SupabaseLobbyService(),
   quizService: new SupabaseQuizService(),
 }
--- src/lib/supahoot/services/lobby-services/supabase-lobby-service.ts
+++
@@ -1,9 +0,0 @@
-import type { LobbyService } from '../lobby-service'
-import { supabase } from '@supahoot/services/supabase-client'
-
-export class SupabaseLobbyService implements LobbyService {
-  async create({ name }: { name: string }) {
-    const response = await supabase.from('lobbies').insert({ name })
-    if (response.error) throw new Error(response.error.message)
-  }
-}

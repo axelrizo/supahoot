71d87d63563ad0169e97377b502c7ab163db8bb9
Axel Rizo <axelg.rizos@gmail.com>
2025-03-13 03:28:17 -0500

    feat(router): add AdminLobby route for managing lobby by ID

--- src/lib/supahoot-web/router/index.ts
+++ src/lib/supahoot-web/router/index.ts
@@ -1,9 +1,13 @@
 import AdminView from '@supahoot-web/views/AdminView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import AdminLobby from '@supahoot-web/views/AdminLobby.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/admin', name: 'admin', component: AdminView }],
+  routes: [
+    { path: '/admin', name: 'admin', component: AdminView },
+    { path: '/admin/lobby/:lobbyId', name: 'admin-lobby', component: AdminLobby },
+  ],
 })
 
 export default router

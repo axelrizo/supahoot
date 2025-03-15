0f7a9fa1811b88bcd904093679bd34ce2e0efc94
Axel Rizo <axelg.rizos@gmail.com>
2025-03-15 12:26:52 -0500

    feat(supabase): add getPlayersByLobby

--- src/lib/supahoot/services/quiz-services/supabase-quiz-service.ts
+++ src/lib/supahoot/services/quiz-services/supabase-quiz-service.ts
@@ -1,7 +1,8 @@
+import type { Lobby } from '@supahoot/quizzes/lobby'
+import type { Quiz } from '@supahoot/quizzes/quiz'
 import type { QuizService } from '@supahoot/services/quiz-service'
 import { supabase } from '@supahoot/services/supabase-client'
-import type { Quiz } from '@supahoot/quizzes/quiz'
-import type { Lobby } from '@supahoot/quizzes/lobby'
+import type { Player } from '../../quizzes/player'
 
 export class SupabaseQuizService implements QuizService {
   async getQuizzes() {
@@ -11,11 +12,20 @@ export class SupabaseQuizService implements QuizService {
 
     return response.data as Quiz[]
   }
+
   async createLobby(quizId: number) {
     const response = await supabase.from('lobbies').insert({ quiz_id: quizId }).select()
 
     if (response.error) throw new Error(response.error.message)
 
     return response.data[0] as Lobby
+  }
+
+  async getPlayersByLobby(_lobbyId: number): Promise<Player[]> {
+    const response = await supabase.from('players').select('*')
+    console.log('🚀 ~ response:', response)
+    if (response.error) throw new Error(response.error.message)
+
+    return response.data as Player[]
   }
 }

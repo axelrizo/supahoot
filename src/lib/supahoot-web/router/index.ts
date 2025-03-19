a4734e46f37280b2ba4463b18783df59c407d363
Axel Rizo <axelg.rizos@gmail.com>
2025-03-19 01:24:15 -0500

    feat(router): switch to createWebHashHistory for improved routing

--- src/lib/supahoot-web/router/index.ts
+++ src/lib/supahoot-web/router/index.ts
@@ -1,10 +1,10 @@
 import AdminLobby from '@supahoot-web/views/AdminLobby.vue'
 import AdminView from '@supahoot-web/views/AdminView.vue'
 import PlayersLobby from '@supahoot-web/views/PlayersLobby.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/admin', name: 'admin', component: AdminView },
     { path: '/admin/lobby/:lobbyId', name: 'admin-lobby', component: AdminLobby },

db7286aef0dce8ffb493d1c3a636d29bf9fca2b3
Axel Rizo <axelg.rizos@gmail.com>
2025-03-19 02:26:08 -0500

    feat(HomeView): implement lobby joining functionality with form submission

---
+++ src/lib/supahoot-web/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const lobbyId = ref('')
+
+const handleSubmitForm = () => {
+  router.push({ name: 'player-lobby', params: { lobbyId: lobbyId.value } })
+}
+</script>
+
+<template>
+  <div>
+    <form data-testid="join-lobby-form" @submit="handleSubmitForm">
+      <input type="text" data-testid="join-lobby-input" v-model="lobbyId" />
+    </form>
+  </div>
+</template>

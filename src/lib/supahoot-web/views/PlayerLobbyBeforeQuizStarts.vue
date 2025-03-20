26121c59cc1d88257081e4d3bbf58a84b9006973
Axel Rizo <axelg.rizos@gmail.com>
2025-03-20 00:26:26 -0500

    feat(player): add PlayerLobbyBeforeQuizStarts component with tests

---
+++ src/lib/supahoot-web/views/PlayerLobbyBeforeQuizStarts.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import type { PlayerProvider } from '@supahoot-web/providers/player-provider'
+import type { NotificationProvider } from '@supahoot-web/providers/notification-provider'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const notificationProvider = inject<NotificationProvider>('notificationProvider')!
+const { player } = inject<PlayerProvider>('playerProvider')!
+
+if (!player) {
+  notificationProvider.showNotification('Error: Player not found')
+  router.push({ name: 'home' })
+}
+</script>
+
+<template>
+  <div>
+    <img :src="player?.image" data-testid="player-avatar" />
+    <span data-testid="player-username">{{ player?.username }}</span>
+  </div>
+</template>

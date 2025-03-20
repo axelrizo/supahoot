5acca686eb718ab7239c8562845c8136507797de
Axel Rizo <axelg.rizos@gmail.com>
2025-03-20 00:21:12 -0500

    feat(router): add home route and update player lobby paths

--- src/lib/supahoot-web/router/index.ts
+++ src/lib/supahoot-web/router/index.ts
@@ -4,6 +4,7 @@ import AdminView from '@supahoot-web/views/AdminView.vue'
 import PlayerProviders from '@supahoot-web/views/PlayerProviders.vue'
 import PlayersLobby from '@supahoot-web/views/PlayersLobby.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -15,13 +16,14 @@ const router = createRouter({
       name: 'admin-quiz',
       component: MockComponent,
     },
+    { path: '/', name: 'home', component: HomeView },
     {
-      path: '',
+      path: '/quiz/:quizId/lobby/:lobbyId',
       component: PlayerProviders,
       children: [
-        { path: '/quiz/:quizId/lobby/:lobbyId', name: 'player-lobby', component: PlayersLobby },
+        { path: '', name: 'player-lobby', component: PlayersLobby },
         {
-          path: '/quiz/:quizId/lobby/:lobbyId/before-start',
+          path: '/before-start',
           name: 'player-lobby-before-quiz-starts',
           component: MockComponent,
         },

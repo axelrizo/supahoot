d9e6468ff43fdaceb166a962580d5617ff146343
Axel Rizo <axelg.rizos@gmail.com>
2025-03-20 20:08:34 -0500

    feat(home): add error handling and notifications for invalid lobby ID in HomeView

--- src/lib/supahoot-web/views/HomeView.test.ts
+++ src/lib/supahoot-web/views/HomeView.test.ts
@@ -1,5 +1,5 @@
 import MockComponent from '@/test/support/MockComponent.vue'
-import { container } from '@/test/support/setup-container-mock'
+import { container, notificationProvider } from '@/test/support/setup-container-mock'
 import { testId } from '@/test/support/utils/html-utils'
 import HomeView from '@supahoot-web/views/HomeView.vue'
 import { mount, type VueWrapper } from '@vue/test-utils'
@@ -11,7 +11,11 @@ let router: RouterMock
 describe('Home', () => {
   beforeEach(() => {
     router = getRouter()
-    router.addRoute({ path: '/quiz/:quizId/lobby/:lobbyId', name: 'player-lobby', component: MockComponent })
+    router.addRoute({
+      path: '/quiz/:quizId/lobby/:lobbyId',
+      name: 'player-lobby',
+      component: MockComponent,
+    })
 
     wrapper = mount(HomeView)
   })
@@ -26,5 +30,21 @@ describe('Home', () => {
       name: 'player-lobby',
       params: { lobbyId: '1', quizId: '1' },
     })
+  })
+
+  test('error: send error notification when lobby id does not exist', async () => {
+    container.quizService.getQuizByLobbyId.mockRejectedValue(new Error('Dummy error'))
+
+    await wrapper.get(testId('join-lobby-input')).setValue('1')
+    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+
+    expect(notificationProvider.showNotification).toHaveBeenCalledWith('Error: Dummy error')
+  })
+
+  test('error: send error notification when lobby id is invalid', async () => {
+    await wrapper.get(testId('join-lobby-input')).setValue('invalid')
+    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+
+    expect(notificationProvider.showNotification).toHaveBeenCalledWith('Error: Invalid lobby id')
   })
 })
--- src/lib/supahoot-web/views/HomeView.vue
+++ src/lib/supahoot-web/views/HomeView.vue
@@ -2,16 +2,30 @@
 import type { ServicesContainer } from '@/lib/supahoot/services/container'
 import { inject, ref } from 'vue'
 import { useRouter } from 'vue-router'
-
+import { type NotificationProvider } from '../providers/notification-provider'
 
 const router = useRouter()
-const container = inject<ServicesContainer>('container')
+
+const container = inject<ServicesContainer>('container')!
+const notificationProvider = inject<NotificationProvider>('notificationProvider')!
 
 const lobbyId = ref('')
 
 const handleSubmitForm = async () => {
-  const quiz = await container.quizService.getQuizByLobbyId(lobbyId.value)
-  await router.push({ name: 'player-lobby', params: { lobbyId: lobbyId.value, quizId: quiz.id } })
+  if (isNaN(parseInt(lobbyId.value))) {
+    notificationProvider.showNotification('Error: Invalid lobby id')
+    return
+  }
+
+  try {
+    const quiz = await container.quizService.getQuizByLobbyId(parseInt(lobbyId.value))
+
+    await router.push({ name: 'player-lobby', params: { lobbyId: lobbyId.value, quizId: quiz.id } })
+  } catch (error) {
+    if (error instanceof Error) {
+      notificationProvider.showNotification(`Error: ${error.message}`)
+    }
+  }
 }
 </script>
 

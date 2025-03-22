4113b5f305c160390a458897e41200d56239e814
Axel Rizo <axelg.rizos@gmail.com>
2025-03-22 17:17:38 -0500

    feat(admin): refactor countdown logic to separate initial and answer countdowns

--- src/lib/supahoot-web/views/AdminQuestionView.test.ts
+++ src/lib/supahoot-web/views/AdminQuestionView.test.ts
@@ -1,3 +1,4 @@
+import type { Question } from '@/lib/supahoot/quizzes/question'
 import { container } from '@/test/support/setup-container-mock'
 import { HTMLUtils } from '@/test/support/utils/html-utils'
 import { mount, type VueWrapper } from '@vue/test-utils'
@@ -7,71 +8,206 @@ import AdminQuestionView from './AdminQuestionView.vue'
 let wrapper: VueWrapper
 let router: RouterMock
 
+const routerParams = { quizId: 1, lobbyId: 2, questionOrder: 3 }
+const question: Question = {
+  id: 1,
+  title: 'Question 1',
+  order: 3,
+  image: 'image.png',
+  answers: [
+    { id: 1, title: 'Answer 1', is_correct: false, order: 1 },
+    { id: 2, title: 'Answer 2', is_correct: false, order: 2 },
+    { id: 3, title: 'Answer 3', is_correct: false, order: 3 },
+    { id: 4, title: 'Answer 4', is_correct: true, order: 4 },
+  ],
+}
+
+const LOADING = HTMLUtils.testId('loading')
+const BEFORE_QUESTION = HTMLUtils.testId('before-question')
+const STARTED_QUESTION = HTMLUtils.testId('started-question')
+
 beforeEach(() => {
   router = getRouter()
-  router.setParams({ quizId: '1', questionOrder: '3', lobbyId: '2' })
-
-  container.quizService.getQuestionByQuizIdAndQuestionOrder.mockResolvedValue({
-    id: 1,
-    title: 'Question 1',
-    order: 3,
-    image: 'image.png',
-    answers: [{ id: 1, title: 'Answer 1', correct: false }],
-  })
+  router.setParams(routerParams)
 })
 
 describe('AdminQuestionView', () => {
   beforeEach(() => {
+    container.quizService.getQuestionByQuizIdAndQuestionOrder.mockResolvedValue(question)
+
     wrapper = mount(AdminQuestionView)
   })
 
   test('success: call service to get question details', () => {
-    expect(container.quizService.getQuestionByQuizIdAndQuestionOrder).toHaveBeenCalledWith(1, 3)
-  })
+    const getQuestionService = container.quizService.getQuestionByQuizIdAndQuestionOrder
 
-  test('success: question title is printed', () => {
-    expect(wrapper.get(HTMLUtils.testId('question-title')).text()).toContain('Question 1')
+    expect(getQuestionService).toHaveBeenCalledWith(routerParams.quizId, routerParams.questionOrder)
   })
 })
 
-describe('AdminQuestionView when has time left', () => {
+describe('AdminQuestionView when does not has question yet', () => {
   beforeEach(() => {
-    container.quizService.listenCountdown.mockImplementation(
-      (_lobbyId: number, callback: (count: number) => void) => {
-        callback(10)
-      },
+    container.quizService.getQuestionByQuizIdAndQuestionOrder.mockResolvedValue(
+      new Promise(() => {}),
     )
 
-    wrapper = mount(AdminQuestionView, { props: { countdownTimeInS: 10 } })
+    wrapper = mount(AdminQuestionView)
   })
 
-  test('success: do not show options', () => {
-    expect(wrapper.find(HTMLUtils.testId('question-answers')).exists()).toBe(false)
+  test('success: show loading question', () => {
+    const loadingSection = wrapper.find(LOADING)
+
+    expect(loadingSection.exists()).toBe(true)
   })
 
-  test('success: show time left', () => {
-    expect(wrapper.get(HTMLUtils.testId('time-left')).text()).toContain('10')
+  test('success: not show before start question section', () => {
+    const beforeStartQuestionContainer = wrapper.find(BEFORE_QUESTION)
+
+    expect(beforeStartQuestionContainer.exists()).toBe(false)
   })
 
-  test('success: do not show question image', () => {
-    expect(wrapper.find(HTMLUtils.testId('question-image')).exists()).toBe(false)
+  test('success: not show started question section', () => {
+    const startedQuestionContainer = wrapper.find(STARTED_QUESTION)
+
+    expect(startedQuestionContainer.exists()).toBe(false)
   })
 })
 
-describe('AdminQuestionView when no has time left', () => {
+describe('AdminQuestionView before start question', () => {
   beforeEach(() => {
-    wrapper = mount(AdminQuestionView, { props: { countdownTimeInS: 0 } })
+    vi.useFakeTimers()
+
+    container.quizService.getQuestionByQuizIdAndQuestionOrder.mockResolvedValue(question)
+
+    wrapper = mount(AdminQuestionView, { props: { initialTimeLeftToStart: 20 } })
   })
 
-  test('success: show question image', async () => {
-    expect(wrapper.get(HTMLUtils.testId('question-image')).attributes('src')).toBe('image.png')
+  afterEach(() => {
+    vi.restoreAllMocks()
   })
 
-  test('success: do not show time left', async () => {
-    expect(wrapper.find(HTMLUtils.testId('time-left')).exists()).toBe(false)
+  test('success: not show loading', () => {
+    const loadingSection = wrapper.find(LOADING)
+
+    expect(loadingSection.exists()).toBe(false)
+  })
+
+  test('success: not show started question section', () => {
+    const startedQuestionContainer = wrapper.find(STARTED_QUESTION)
+
+    expect(startedQuestionContainer.exists()).toBe(false)
+  })
+
+  test('success: show before start question section', () => {
+    const beforeStartQuestionContainer = wrapper.find(BEFORE_QUESTION)
+
+    expect(beforeStartQuestionContainer.exists()).toBe(true)
+  })
+
+  test('success: show question title', () => {
+    const beforeStartQuestionTitle = wrapper.get(
+      `${BEFORE_QUESTION} > ${HTMLUtils.testId('question-title')}`,
+    )
+
+    expect(beforeStartQuestionTitle.text()).toContain(question.title)
+  })
+
+  test('success: show time left to start', () => {
+    const timeLeft = wrapper.get(`${BEFORE_QUESTION} > ${HTMLUtils.testId('time-left')}`)
+
+    expect(timeLeft.text()).toContain('20')
+  })
+
+  test('success: call the service to update countdown', async () => {
+    const updateCountdownService = container.quizService.updateCountdownBeforeQuestionStart
+
+    vi.advanceTimersToNextTimer()
+    expect(updateCountdownService).toHaveBeenCalledWith(routerParams.lobbyId, 20)
+
+    vi.advanceTimersToNextTimer()
+    expect(updateCountdownService).toHaveBeenCalledWith(routerParams.lobbyId, 19)
+  })
+})
+
+describe('AdminQuestionView when has started', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+
+    container.quizService.getQuestionByQuizIdAndQuestionOrder.mockResolvedValue(question)
+
+    wrapper = mount(AdminQuestionView, {
+      props: { initialTimeLeftToStart: 0, initialTimeLeftToAnswer: 20 },
+    })
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  test('success: not show loading', () => {
+    const loadingContainer = wrapper.find(LOADING)
+
+    expect(loadingContainer.exists()).toBe(false)
+  })
+
+  test('success: not show before start question section', () => {
+    const beforeStartQuestionContainer = wrapper.find(BEFORE_QUESTION)
+
+    expect(beforeStartQuestionContainer.exists()).toBe(false)
+  })
+
+  test('success: show started question section', () => {
+    const startedQuestionContainer = wrapper.find(STARTED_QUESTION)
+
+    expect(startedQuestionContainer.exists()).toBe(true)
+  })
+
+  test("success: show question's title", () => {
+    const questionTitle = wrapper.find(
+      `${STARTED_QUESTION} > ${HTMLUtils.testId('question-title')}`,
+    )
+
+    expect(questionTitle.text()).toContain(question.title)
+  })
+
+  test('success: show question image', () => {
+    const questionImage = wrapper.get(`${STARTED_QUESTION} > ${HTMLUtils.testId('question-image')}`)
+
+    expect(questionImage.attributes('src')).toBe(question.image)
+  })
+
+  test('success: show time to answer', () => {
+    const timeToAnswer = wrapper.get(`${STARTED_QUESTION} > ${HTMLUtils.testId('time-to-answer')}`)
+
+    expect(timeToAnswer.text()).toContain('20')
   })
 
   test('success: show answers', async () => {
-    expect(wrapper.find(HTMLUtils.testId('answer-title')).text()).toBe('Answer 1')
+    const answers = wrapper.findAll(`${STARTED_QUESTION} > ${HTMLUtils.testId('answer')}`)
+
+    expect(answers).toHaveLength(question.answers.length)
+  })
+
+  test('success: answers print title', () => {
+    const answers = wrapper.findAll(
+      `${STARTED_QUESTION} > ${HTMLUtils.testId('answer')} > ${HTMLUtils.testId('answer-title')}`,
+    )
+
+    answers.forEach((answer, index) => {
+      expect(answer.text()).toContain(question.answers[index].title)
+    })
+  })
+
+  test("success: call the service to update countdown before question's start", async () => {
+    const updateCountdownService = container.quizService.updateStartAnswerQuestionCountdown
+
+    // finish the countdown to start the question
+    vi.advanceTimersToNextTimer()
+
+    vi.advanceTimersToNextTimer()
+    expect(updateCountdownService).toHaveBeenCalledWith(routerParams.lobbyId, 20)
+
+    vi.advanceTimersToNextTimer()
+    expect(updateCountdownService).toHaveBeenCalledWith(routerParams.lobbyId, 19)
   })
 })
--- src/lib/supahoot-web/views/AdminQuestionView.vue
+++ src/lib/supahoot-web/views/AdminQuestionView.vue
@@ -6,13 +6,21 @@ import { useRoute } from 'vue-router'
 
 const UPDATE_COUNTER_INTERVAL_MS = 1000
 
-const { countdownTimeInS } = defineProps({
-  countdownTimeInS: {
+const { initialTimeLeftToStart, initialTimeLeftToAnswer } = defineProps({
+  initialTimeLeftToStart: {
     type: Number,
     required: false,
     default: () => {
-      const INITIAL_COUNTDOWN_VALUE_IN_S = 10
-      return INITIAL_COUNTDOWN_VALUE_IN_S
+      const COUNTDOWN_IN_S = 10
+      return COUNTDOWN_IN_S
+    },
+  },
+  initialTimeLeftToAnswer: {
+    type: Number,
+    required: false,
+    default: () => {
+      const COUNTDOWN_IN_S = 10
+      return COUNTDOWN_IN_S
     },
   },
 })
@@ -26,7 +34,8 @@ const lobbyId = parseInt(route.params.lobbyId as string)
 const container = inject<ServicesContainer>('container')!
 
 const question = ref<Question | null>(null)
-const countdown = ref(countdownTimeInS)
+const timeLeftToStart = ref(initialTimeLeftToStart)
+const timeLeftToAnswer = ref(initialTimeLeftToAnswer)
 
 onMounted(async () => {
   question.value = await container.quizService.getQuestionByQuizIdAndQuestionOrder(
@@ -34,33 +43,42 @@ onMounted(async () => {
     questionOrder,
   )
 
-  const countdownInterval = setInterval(updateCountdown, UPDATE_COUNTER_INTERVAL_MS)
-
-  function updateCountdown() {
-    if (countdown.value === 0) {
-      clearInterval(countdownInterval)
+  const beforeQuestionCountdownInterval = setInterval(() => {
+    if (timeLeftToStart.value === 0) {
+      startAnswerQuestionsInterval()
+      clearInterval(beforeQuestionCountdownInterval)
       return
     }
 
-    container.quizService.updateCountdown(lobbyId, countdown.value--)
+    container.quizService.updateCountdownBeforeQuestionStart(lobbyId, timeLeftToStart.value--)
+  }, UPDATE_COUNTER_INTERVAL_MS)
+
+  const startAnswerQuestionsInterval = () => {
+    const timeLeftToAnswerCountdownInterval = setInterval(() => {
+      if (timeLeftToAnswer.value === 0) {
+        clearInterval(timeLeftToAnswerCountdownInterval)
+        return
+      }
+
+      container.quizService.updateStartAnswerQuestionCountdown(lobbyId, timeLeftToAnswer.value--)
+    }, UPDATE_COUNTER_INTERVAL_MS)
   }
 })
 </script>
 
 <template>
   <div>
-    <div v-if="question">
-      <div data-testid="question-title">Question 1</div>
-      <img v-if="countdown === 0" data-testid="question-image" :src="question.image" />
+    <div v-if="!question" data-testid="loading"></div>
+    <div v-else-if="timeLeftToStart > 0" data-testid="before-question">
+      <div data-testid="time-left">{{ timeLeftToStart }}</div>
+      <div data-testid="question-title">{{ question.title }}</div>
     </div>
-    <div v-if="countdown > 0">
-      <div data-testid="time-left">{{ countdown }}</div>
-    </div>
-    <div v-if="countdown === 0 && question">
-      <div data-testid="question-answers">
-        <div v-for="answer in question.answers" :key="answer.id">
-          <div data-testid="answer-title">{{ answer.title }}</div>
-        </div>
+    <div v-else data-testid="started-question">
+      <div data-testid="time-to-answer">{{ timeLeftToAnswer }}</div>
+      <div data-testid="question-title">{{ question.title }}</div>
+      <img data-testid="question-image" :src="question.image" />
+      <div v-for="answer in question.answers" :key="answer.id" data-testid="answer">
+        <div data-testid="answer-title">{{ answer.title }}</div>
       </div>
     </div>
   </div>

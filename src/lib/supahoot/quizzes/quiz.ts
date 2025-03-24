f5791a55652a6d090d998421e3312b6a6af63627
Axel Rizo <axelg.rizos@gmail.com>
2025-03-24 01:27:32 -0500

    refactor(quizzes): update Answer interface for camelCase and add QuestionWithAnswers type

--- src/lib/supahoot/quizzes/quiz.ts
+++ src/lib/supahoot/quizzes/quiz.ts
@@ -1,4 +1,10 @@
+import type { QuestionWithAnswers } from '@supahoot/quizzes/question'
+
 export interface Quiz {
   name: string
   id: number
+}
+
+export type QuizWithQuestionsWithAnswers = Quiz & {
+  questions: QuestionWithAnswers[]
 }

2a921591920adaed65ed9d75a616cfa8a0de7911
Axel Rizo <axelg.rizos@gmail.com>
2025-03-23 22:43:37 -0500

    refactor(router): remove unused AdminQuestionView route

--- src/lib/supahoot-web/router/index.ts
+++ src/lib/supahoot-web/router/index.ts
@@ -5,7 +5,6 @@ import PlayerLobbyBeforeQuizStarts from '@supahoot-web/views/PlayerLobbyBeforeQu
 import PlayerProviders from '@supahoot-web/views/PlayerProviders.vue'
 import PlayersLobby from '@supahoot-web/views/PlayersLobby.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
-import AdminQuestionView from '../views/AdminQuestionView.vue'
 import PlayerQuestionView from '../views/PlayerQuestionView.vue'
 
 const router = createRouter({
@@ -13,11 +12,6 @@ const router = createRouter({
   routes: [
     { path: '/admin', name: 'admin', component: AdminView },
     { path: '/admin/quiz/:quizId/lobby/:lobbyId', name: 'admin-lobby', component: AdminLobby },
-    {
-      path: '/admin/quiz/:quizId/lobby/:lobbyId/question/:questionOrder',
-      name: 'admin-quiz',
-      component: AdminQuestionView,
-    },
     { path: '/', name: 'home', component: HomeView },
     {
       path: '/quiz/:quizId/lobby/:lobbyId',

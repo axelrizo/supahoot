6ea7b839a5ba3072a52904574e37bdbbf1283773
Axel Rizo <axelg.rizos@gmail.com>
2026-04-24 12:10:14 -0500

    tets(PlayersLobby): remove unused import

--- src/lib/supahoot-web/views/PlayersLobby.test.ts
+++ src/lib/supahoot-web/views/PlayersLobby.test.ts
@@ -1,5 +1,4 @@
 import { FileUtils } from '@/lib/supahoot/utils/file.utils'
-import MockComponent from '@/test/support/MockComponent.vue'
 import {
   container,
   notificationProvider,
@@ -7,7 +6,7 @@ import {
 } from '@/test/support/setup-container-mock'
 import { testId } from '@/test/support/utils/html-utils'
 import { flushPromises, mount, type VueWrapper } from '@vue/test-utils'
-import { getRouter, type RouterMock } from 'vue-router-mock'
+import { getRouter } from 'vue-router-mock'
 import PlayersLobby from './PlayersLobby.vue'
 
 describe('PlayersLobbyView', () => {

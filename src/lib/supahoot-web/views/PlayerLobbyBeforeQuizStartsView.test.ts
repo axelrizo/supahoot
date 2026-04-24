50937941bd3175f921321fd5e35096239949442c
Axel Rizo <axelg.rizos@gmail.com>
2026-04-24 16:10:10 -0500

    test(PlayerBeforeQuiz): refactor tests to keep them simple

--- src/lib/supahoot-web/views/PlayerLobbyBeforeQuizStartsView.test.ts
+++ src/lib/supahoot-web/views/PlayerLobbyBeforeQuizStartsView.test.ts
@@ -1,73 +1,66 @@
-import MockComponent from '@/test/support/MockComponent.vue'
-import {
-  container,
-  notificationProvider,
-  playerProvider,
-} from '@/test/support/setup-container-mock'
+import { container, playerProvider, } from '@/test/support/setup-container-mock'
 import { testId } from '@/test/support/utils/html-utils'
 import { mount, VueWrapper } from '@vue/test-utils'
-import { getRouter, type RouterMock } from 'vue-router-mock'
+import { getRouter } from 'vue-router-mock'
 import PlayerLobbyBeforeQuizStartsView from './PlayerLobbyBeforeQuizStartsView.vue'
+import { createPlayer } from '@/test/support/utils/factory-utils'
+import { expectErrorNotification } from '@/test/support/utils/expect-utils'
 
-let wrapper: VueWrapper
-let router: RouterMock
+
+const mountPlayerLobbyBeforeQuizStartsView = (): VueWrapper => mount(PlayerLobbyBeforeQuizStartsView)
+
+const playerIsInLobbyWithQuiz = async (quizId: number, lobbyId: number) => {
+  await getRouter().push({ params: { quizId, lobbyId } })
+}
 
 describe('PlayerLobbyBeforeQuizStartsView', () => {
-  beforeEach(() => {
-    router = getRouter()
-    router.setParams({ lobbyId: '1', quizId: '1' })
-    router.addRoute({
-      name: 'player-quizz',
-      path: '/quiz/:quizId/lobby/:lobbyId/quiz-started',
-      component: MockComponent,
-    })
-
-    playerProvider.player = { id: 1, username: 'Player 1', image: '/dummy_avatar.png' }
-
-    container.quizService.listenQuizStart.mockImplementation(
-      (_lobbyId: number, callback: () => void) => {
-        callback()
-      },
-    )
-
-    wrapper = mount(PlayerLobbyBeforeQuizStartsView)
-  })
-
   describe('when player is in lobby', () => {
     test('shows player avatar', () => {
-      expect(wrapper.get(testId('player-avatar')).attributes('src')).toBe('/dummy_avatar.png')
+      const player = createPlayer()
+      playerProvider.player = player
+      const playerLobbyView = mountPlayerLobbyBeforeQuizStartsView()
+
+      expect(playerLobbyView.get(testId('player-avatar')).attributes('src')).toBe(player.image)
     })
 
     test('shows player username', () => {
-      expect(wrapper.get(testId('player-username')).text()).toBe('Player 1')
+      const player = createPlayer()
+      playerProvider.player = player
+      const playerLobbyView = mountPlayerLobbyBeforeQuizStartsView()
+
+      expect(playerLobbyView.get(testId('player-username')).text()).toBe(player.username)
     })
   })
 
   describe('when quiz starts', () => {
-    test("redirects to player's lobby", async () => {
-      expect(router.push).toHaveBeenLastCalledWith({
-        name: 'player-quiz',
-        params: { quizId: 1, lobbyId: 1 },
-      })
+    test("redirects to player's quiz", async () => {
+      playerIsInLobbyWithQuiz(1, 10)
+      container.quizService.listenQuizStart.mockImplementation(
+        (_lobbyId: number, callback: () => void) => {
+          callback()
+        },
+      )
+      const playerLobbyView = mountPlayerLobbyBeforeQuizStartsView()
+      expect(playerLobbyView.router.push).toHaveBeenLastCalledWith(
+        expect.objectContaining({ name: 'player-quiz', params: { quizId: 1, lobbyId: 10 }, })
+      )
+    })
+  })
+
+  describe('when player info is not provided', () => {
+    test('shows error message', () => {
+      playerProvider.player = null
+      mountPlayerLobbyBeforeQuizStartsView()
+
+      expectErrorNotification('Player not found')
+    })
+
+    test('redirects to home', () => {
+      playerProvider.player = null
+      const playerLobbyView = mountPlayerLobbyBeforeQuizStartsView()
+
+      expect(playerLobbyView.router.push).toHaveBeenCalledWith(expect.objectContaining({ name: 'home' }))
     })
   })
 })
 
-describe('PlayerLobbyBeforeQuizStarts when player is not provided', () => {
-  beforeEach(() => {
-    router = getRouter()
-    router.addRoute({ path: '/', component: MockComponent, name: 'home' })
-
-    playerProvider.player = null
-
-    wrapper = mount(PlayerLobbyBeforeQuizStartsView)
-  })
-
-  test('error: shows error message when player is not provided', () => {
-    expect(notificationProvider.showNotification).toBeCalledWith('Error: Player not found')
-  })
-
-  test('error: redirect to home when player is not provided', () => {
-    expect(router.currentRoute.value).toMatchObject({ name: 'home' })
-  })
-})

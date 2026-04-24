7d2c12af6cd87c7bddbd212d933ec51c1a10a5fe
Axel Rizo <axelg.rizos@gmail.com>
2026-04-24 01:27:16 -0500

    test(HomeView): refactor test to make them more readable

--- src/lib/supahoot-web/views/HomeView.test.ts
+++ src/lib/supahoot-web/views/HomeView.test.ts
@@ -8,7 +8,7 @@ import { getRouter, type RouterMock } from 'vue-router-mock'
 let wrapper: VueWrapper
 let router: RouterMock
 
-describe('Home', () => {
+describe('HomeView', () => {
   beforeEach(() => {
     router = getRouter()
     router.addRoute({
@@ -20,31 +20,89 @@ describe('Home', () => {
     wrapper = mount(HomeView)
   })
 
-  test('success: player can join a lobby with the lobby id', async () => {
-    container.quizService.getQuizByLobbyId.mockResolvedValue({ id: 1, name: 'Quiz 1' })
+  describe('when lobby exists and user joins', () => {
+    test('redirects to lobby', async () => {
+      serviceHasLobbyWithQuizzId(1)
 
-    await wrapper.get(testId('join-lobby-input')).setValue('1')
-    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+      await userJoinsLobby(1)
 
-    expect(router.currentRoute.value).toMatchObject({
-      name: 'player-lobby',
-      params: { lobbyId: '1', quizId: '1' },
+      expectRedirectUserToLobbyWithQuiz(1, 1)
     })
   })
 
-  test('error: send error notification when lobby id does not exist', async () => {
-    container.quizService.getQuizByLobbyId.mockRejectedValue(new Error('Dummy error'))
+  describe('when service throws an error', () => {
+    test('sends error notification', async () => {
+      quizServiceThrowsError('Dummy error')
 
-    await wrapper.get(testId('join-lobby-input')).setValue('1')
-    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+      await userJoinsLobby(1)
 
-    expect(notificationProvider.showNotification).toHaveBeenCalledWith('Error: Dummy error')
+      expectErrorNotification('Dummy error')
+    })
   })
 
-  test('error: send error notification when lobby id is invalid', async () => {
-    await wrapper.get(testId('join-lobby-input')).setValue('invalid')
-    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+  describe('when lobby id is invalid', () => {
+    test('sends error notification', async () => {
+      await userJoinsLobbyWithString('invalid')
 
-    expect(notificationProvider.showNotification).toHaveBeenCalledWith('Error: Invalid lobby id')
+      expectErrorNotification('Invalid lobby id')
+    })
   })
+
+  /**
+   * Mocks the quiz service to return a quiz for the given lobby id
+   * @param quizId The quiz id returned by the service
+   */
+  const serviceHasLobbyWithQuizzId = (quizId: number): void => {
+    container.quizService.getQuizByLobbyId.mockResolvedValue({ id: quizId, name: 'Any Quiz Name' })
+    return
+  }
+
+  /**
+    * Mocks the quiz service to throw an error when trying to get a quiz by lobby id
+    * @param error The error message to throw
+    */
+  const quizServiceThrowsError = (error: string): void => {
+    container.quizService.getQuizByLobbyId.mockRejectedValue(new Error(error))
+    return
+  }
+
+  /**
+   * Simulates the user joining a lobby by filling the form and submitting it
+   * @param lobbyId The lobby id to join
+   */
+  const userJoinsLobby = async (lobbyId: number): Promise<void> => {
+    await wrapper.get(testId('join-lobby-input')).setValue(lobbyId.toString())
+    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+    return
+  }
+
+  /**
+   * Simulates the user joining a lobby by filling the form and submitting it with a string value
+   * @param lobbyId The lobby id to join as a string
+   */
+  const userJoinsLobbyWithString = async (lobbyId: string): Promise<void> => {
+    await wrapper.get(testId('join-lobby-input')).setValue(lobbyId)
+    await wrapper.get(testId('join-lobby-form')).trigger('submit')
+    return
+  }
+
+  /**
+   * Expects the user to receive an error notification
+   * @param error The error message in the notification
+   */
+  const expectErrorNotification = (error: string): void => {
+    expect(notificationProvider.showNotification).toHaveBeenCalledWith(`Error: ${error}`)
+  }
+
+  /**
+   * User is redirected to the lobby and quiz
+   * @param lobbyId The lobby id the user should be redirected to
+   * @param quizId The quiz id the user should be redirected to
+   */
+  const expectRedirectUserToLobbyWithQuiz = (lobbyId: number, quizId: number): void => {
+    expect(router.currentRoute.value).toMatchObject({
+      name: 'player-lobby',
+      params: { lobbyId: lobbyId.toString(), quizId: quizId.toString() },
+    })
+  }
 })

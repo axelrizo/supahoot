b9f5a8a7b5f00d1495a9dae6f616b7407aa690b3
Axel Rizo <axelg.rizos@gmail.com>
2026-04-26 20:08:56 -0500

    test(expect): rename from sendsErrorNotification to showErrorNotification

--- src/test/support/utils/expect-utils.ts
+++ src/test/support/utils/expect-utils.ts
@@ -5,6 +5,6 @@ import { notificationProvider } from '@/test/support/setup-container-mock'
  * Expects the user to receive an error notification
  * @param error The error message in the notification
  */
-export const sendsErrorNotification = (error: string): void => {
+export const showsErrorNotification = (error: string): void => {
   expect(notificationProvider.showNotification).toHaveBeenCalledWith(`Error: ${error}`)
 }
